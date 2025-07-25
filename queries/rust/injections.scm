550492b0b6a477fd94ce2c11de79c78aaf1bdabb
Dallin Huff <dallinhuff@gmail.com>
2025-07-24 21:04:05 -0600

    tree-sitter: Update doc-comment injection query for rust

--- queries/rust/injections.scm
+++ queries/rust/injections.scm
@@ -1,6 +1,16 @@
-((line_comment) @_first
- (_) @rust
- (line_comment) @_last
- (#match? @_first "^/// ```$")
- (#match? @_last "^/// ```$")
- (#offset! @rust 0 4 0 4))
+([(line_comment !doc) (block_comment !doc)] @injection.content
+ (#set! injection.language "comment"))
+
+((doc_comment) @injection.content
+ (#set! injection.language "markdown-rustdoc")
+ (#set! injection.combined))
+
+((macro_invocation
+  (token_tree) @injection.content)
+ (#set! injection.language "rust")
+ (#set! injection.include-children))
+
+((macro_rule
+  (token_tree) @injection.content)
+ (#set! injection.language "rust")
+ (#set! injection.include-children))
