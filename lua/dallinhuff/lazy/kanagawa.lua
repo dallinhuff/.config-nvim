19adcea293a436ddfec65442874292808ed173f4
Dallin Huff <me@dallinhuff.com>
2026-01-26 22:15:03 -0700

    kanagawa: Customize diagnostics & cmp menu

--- lua/dallinhuff/lazy/kanagawa.lua
+++ lua/dallinhuff/lazy/kanagawa.lua
@@ -2,7 +2,31 @@ return {
   'rebelot/kanagawa.nvim',
   lazy = false,
   priority = 1000,
-  config = function()
-    require('kanagawa').load 'wave'
+  config = function(_, opts)
+    require('kanagawa').setup(opts)
+    vim.cmd 'colorscheme kanagawa-wave'
   end,
+  opts = {
+    overrides = function(colors)
+      local theme = colors.theme
+      local makeDiagnosticColor = function(color)
+        local c = require 'kanagawa.lib.color'
+        return { fg = color, bg = c(color):blend(theme.ui.bg, 0.95):to_hex() }
+      end
+
+      return {
+        -- add tinted background behind diagnostics
+        DiagnosticVirtualTextHint = makeDiagnosticColor(theme.diag.hint),
+        DiagnosticVirtualTextInfo = makeDiagnosticColor(theme.diag.info),
+        DiagnosticVirtualTextWarn = makeDiagnosticColor(theme.diag.warning),
+        DiagnosticVirtualTextError = makeDiagnosticColor(theme.diag.error),
+
+        -- darken complete suggestions
+        Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
+        PmenuSel = { fg = 'NONE', bg = theme.ui.bg_p2 },
+        PmenuSbar = { bg = theme.ui.bg_m1 },
+        PmenuThumb = { bg = theme.ui.bg_p2 },
+      }
+    end,
+  },
 }
