45582cdd8ed7264bcaf78faa3f6c1b318d2409d5
Dallin Huff <me@dallinhuff.com>
2026-02-15 14:07:42 -0700

    lsp: Simplify diagnostics config and show float on jump

--- lua/dallinhuff/lazy/lsp-config.lua
+++ lua/dallinhuff/lazy/lsp-config.lua
@@ -86,6 +86,7 @@ return {
     })
 
     vim.diagnostic.config {
+      update_in_insert = false,
       severity_sort = true,
       float = { border = 'rounded', source = 'if_many' },
       underline = { severity = vim.diagnostic.severity.ERROR },
@@ -97,19 +98,9 @@ return {
           [vim.diagnostic.severity.HINT] = '󰌶 ',
         },
       } or {},
-      virtual_text = {
-        source = 'if_many',
-        spacing = 2,
-        format = function(diagnostic)
-          local diagnostic_message = {
-            [vim.diagnostic.severity.ERROR] = diagnostic.message,
-            [vim.diagnostic.severity.WARN] = diagnostic.message,
-            [vim.diagnostic.severity.INFO] = diagnostic.message,
-            [vim.diagnostic.severity.HINT] = diagnostic.message,
-          }
-          return diagnostic_message[diagnostic.severity]
-        end,
-      },
+      virtual_text = true,
+      virtual_lines = false,
+      jump = { float = true },
     }
 
     ---@class LspServersConfig
